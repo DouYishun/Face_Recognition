e1cee2175aab882b3f1d9a7cd2aa0c74b1ce2591
DouYishun <yishun.dou@gmail.com>
2018-02-06 15:13:55 +0800

    1

--- faceRecognition/src/main/java/org/hipi/covar/MeanMapper.java
+++ faceRecognition/src/main/java/org/hipi/covar/MeanMapper.java
@@ -13,8 +13,8 @@ import org.bytedeco.javacpp.opencv_core.Scalar;
 
 import java.io.IOException;
 
-public class MeanMapper extends 
-  Mapper<HipiImageHeader, FloatImage, IntWritable, OpenCVMatWritable> {
+public class MeanMapper extends
+        Mapper<HipiImageHeader, FloatImage, IntWritable, OpenCVMatWritable> {
 
   @Override
   public void map(HipiImageHeader header, FloatImage image, Context context)
@@ -25,10 +25,10 @@ public class MeanMapper extends
     
     // if unable to convert input FloatImage to grayscale Mat, skip image and move on
     if(!Covariance.convertFloatImageToGrayscaleMat(image, cvImage)) {
-      System.out.println("MeanMapper is skipping image with invalid color space.");
-      return;
+        System.out.println("MeanMapper is skipping image with invalid color space.");
+        return;
     }
-    
+
 
     /* Compute mean using OpenCV */
     // patch dimensions (N X N)
@@ -41,12 +41,12 @@ public class MeanMapper extends
 
     // collect patches and add their values to mean patch mat
     for (int i = 0; i < iMax; i++) {
-      int x = ((cvImage.cols() - N) * i) / iMax;
-      for (int j = 0; j < jMax; j++) {
-        int y = ((cvImage.rows() - N) * j) / jMax;
-        Mat patch = cvImage.apply(new Rect(x, y, N, N));
-        opencv_core.add(patch, mean, mean);
-      }
+        int x = ((cvImage.cols() - N) * i) / iMax;
+        for (int j = 0; j < jMax; j++) {
+            int y = ((cvImage.rows() - N) * j) / jMax;
+            Mat patch = cvImage.apply(new Rect(x, y, N, N));
+            opencv_core.add(patch, mean, mean);
+        }
     }
     
     // scale mean patch mat based on total number of patches
